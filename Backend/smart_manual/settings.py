24f9b390e2185a454fc12ef957e5d043bf60d6b0
tayyababdullah-9903 <tayyababdullah29@gmail.com>
2025-10-18 15:30:10 +0500

    sdad

--- Backend/smart_manual/settings.py
+++ Backend/smart_manual/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'parser',
+    'manuals',
+    'integration',
+    'chatbot',
+    'uml',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
